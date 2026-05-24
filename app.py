a8f4efb375e226d757b0ad33cfb97bea59b6bbb1
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-24 14:19:50 +0300

    Update app.py
    Try mobile web YouTube client

--- app.py
+++ app.py
@@ -154,22 +154,22 @@ def extract_youtube_hls(url: str):
     "format": "95/best[protocol=m3u8]/best",
     "http_headers": {
         "User-Agent": (
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+            "Mozilla/5.0 (Linux; Android 13; Mobile) "
             "AppleWebKit/537.36 (KHTML, like Gecko) "
-            "Chrome/125.0.0.0 Safari/537.36"
+            "Chrome/125.0.0.0 Mobile Safari/537.36"
         ),
         "Accept-Language": "en-US,en;q=0.9",
-        "Referer": "https://www.youtube.com/"
+        "Referer": "https://m.youtube.com/"
     },
     "extractor_args": {
         "youtube": {
             "player_client": [
+                "mweb",
                 "web"
             ]
         }
     }
 }
-
     if cookie_file:
         ydl_opts["cookiefile"] = cookie_file
 
